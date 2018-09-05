c1390ffc20b3c3fc10a9bb8b3066e8a2b8455043
salaboy <salaboy@gmail.com>
2018-09-05 17:16:37 +0100

    adding simple endpoints

--- src/main/java/org/spring/cloud/k8s/ticketsservice/TicketsServiceApplication.java
+++ src/main/java/org/spring/cloud/k8s/ticketsservice/TicketsServiceApplication.java
@@ -2,11 +2,19 @@ package org.spring.cloud.k8s.ticketsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TicketsServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketsServiceApplication.class, args);
+	}
+
+	@GetMapping
+	public String hello(){
+		return "Hello from Tickets";
 	}
 }
