87ef6a2d773211ec3cb29f0850556f470782c538
salaboy <salaboy@gmail.com>
2018-09-06 11:27:01 +0100

    adding admin client

--- src/main/java/org/spring/cloud/k8s/ticketsservice/TicketsServiceApplication.java
+++ src/main/java/org/spring/cloud/k8s/ticketsservice/TicketsServiceApplication.java
@@ -2,6 +2,9 @@ package org.spring.cloud.k8s.ticketsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +19,14 @@ public class TicketsServiceApplication {
 	@GetMapping
 	public String hello(){
 		return "Hello from Tickets";
+	}
+
+	@Configuration
+	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().permitAll()
+					.and().csrf().disable();
+		}
 	}
 }
