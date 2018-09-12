aab5a6c7cc53b099097e974d034631f15eb5b900
salaboy <salaboy@gmail.com>
2018-09-12 08:54:32 +0100

    adding available tickets service

--- src/main/java/org/spring/cloud/k8s/ticketsservice/TicketsServiceApplication.java
+++ src/main/java/org/spring/cloud/k8s/ticketsservice/TicketsServiceApplication.java
@@ -12,21 +12,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TicketsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TicketsServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TicketsServiceApplication.class, args);
+    }
 
-	@GetMapping
-	public String hello(){
-		return "Hello from Tickets";
-	}
+    @GetMapping()
+    public String hello() {
+        return "Hello from Tickets";
+    }
 
-	@Configuration
-	public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.authorizeRequests().anyRequest().permitAll()
-					.and().csrf().disable();
-		}
-	}
+    @GetMapping(path = "/tickets")
+    public int getAvailableTickets() {
+        return 1000;
+    }
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
+    }
 }
